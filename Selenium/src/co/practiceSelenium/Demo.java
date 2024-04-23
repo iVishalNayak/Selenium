4985200e34d76f0e2dadc6f8e0a910c0256a9d3f
iVishalNayak <42730507+iVishalNayak@users.noreply.github.com>
2024-04-23 17:03:30 +0530

    Add files via upload

---
+++ Selenium/src/co/practiceSelenium/Demo.java
@@ -0,0 +1,20 @@
+package co.practiceSelenium;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+
+public class Demo {
+
+	public static void main(String[] args) {
+	System.setProperty("webdriver.chrome.driver", "F:\\Eclipse\\Selenium\\driver\\chromedriver.exe");
+    WebDriver driver=new ChromeDriver();
+		
+    driver.get("https://google.com");
+    
+    
+ 
+
+	}
+
+}
